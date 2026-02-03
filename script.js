a9fae1979be501be90bfcc59d56b4c4f68bcca05
pastoralyancelean-create <pastoralyancelean@gmail.com>
2026-02-03 20:27:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
+// DARK MODE TOGGLE
 const themeBtn = document.getElementById("themeToggle");
 
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
-  // Change button icon depending on mode
   if(document.body.classList.contains("dark-mode")) {
-    themeBtn.textContent = "☀️"; // Sun icon for light mode
+    themeBtn.textContent = "☀️"; // sun icon for light mode
   } else {
-    themeBtn.textContent = "🌓"; // Moon icon for dark mode
+    themeBtn.textContent = "🌓"; // moon icon for dark mode
   }
 });
 
+// HAMBURGER MENU
 function toggleMenu() {
   document.getElementById("nav-links").classList.toggle("show");
 }
-
 function closeMenu() {
   document.getElementById("nav-links").classList.remove("show");
 }
 
+// PRODUCT PRICE TOGGLE
 function togglePrice(el) {
   const price = el.querySelector(".price");
   price.style.display = price.style.display === "inline-block" ? "none" : "inline-block";
